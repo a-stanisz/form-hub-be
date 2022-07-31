63611c2ab3353f04d08924365985b8cb5ca65c72
a-stanisz <hello@adamstaniszewski.com>
2022-07-31 21:49:07 +0200

    Add knex migration, update Prisma schema

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,44 @@
 import { PrismaClient } from '@prisma/client';
-// import { randomUUID } from 'crypto';
+import { randomUUID } from 'crypto';
 import express from 'express';
 import morgan from 'morgan';
+// import { nanoid } from 'nanoid';
 
 const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+const genId = () => randomUUID();
 
 const seedDB = async () => {
-  if (!(await db.user.count())) {
-    await db.user.createMany({
+  if ((await db.submission.count()) === 0) {
+    await db.submission.createMany({
       data: [
         {
-          email: 'email@example.com',
-          name: 'Admin',
+          id: genId(),
+          submittedAt: new Date(),
+          data: {"name": "John Doe", "twitter": "@example"}
         },
         {
-          email: 'john.doe@example.com',
-          name: 'John Doe',
-        },
+          id: genId(),
+          submittedAt: new Date(),
+          data: {"name": "Jane Doe", "twitter": "@exemplo"}
+        }
       ],
     });
   }
 };
-seedDB();
+
+try {
+  seedDB();
+} catch (error) {
+  console.log(error);
+}
 
 const app = express();
 const port = Number(process.env.PORT ?? 8080);
 app.use(morgan('dev'));
 
 app.get('/', async (req, res) => {
-  const users = await db.user.findMany();
-  res.json(users);
+  const submissions = await db.submission.findMany();
+  res.json(submissions);
 });
 
 app.listen(port, '0.0.0.0', () => {
